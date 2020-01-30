b42407b77015725c2c42d7e9db533052c9bbb230
aliciaschreiner <aliciaschreiner@gmail.com>
2020-01-30 14:49:42 -0800

    1

---
+++ lesson4/js/preston-4-script.js
@@ -0,0 +1,12 @@
+var now = new Date();
+var month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var fdate = day[now.getDay()]+", " + month[now.getMonth()] +" "+ now.getDate() + ", " + now.getFullYear();
+document.getElementById("currentdate").innerHTML=fdate;
+
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+document.querySelector(".navigation").classList.toggle("responsive");
+}
