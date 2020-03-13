903013d521971b94f5ffcd963313ffdc064cde19
aliciaschreiner <aliciaschreiner@gmail.com>
2020-03-13 11:19:04 -0700

    a

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,14 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=e2c5cf2d76014478c0c017d1d040b23c";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  
+    const desc = jsObject.weather[0].description;  
+    document.getElementById('imagesrc').textContent = imagesrc;  
+    document.getElementById('icon').setAttribute('src', imagesrc); 
+    document.getElementById('icon').setAttribute('alt', desc);    
+  });
