8c29a83318d2e6d53c299be43e31e828f9d36ef1
aliciaschreiner <aliciaschreiner@gmail.com>
2020-03-20 13:39:51 -0700

    a

--- lesson11/js/fishhaven-11.js
+++ lesson11/js/fishhaven-11.js
@@ -17,40 +17,31 @@ fetch(apiURL)
 /* Events from Town Data */
 
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-
+ 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  
-
+  
+  .then(function (jsonObject) {      
     const towns = jsonObject['towns'];
+   for (let i = 0; i < towns.length; i++ ) {
+       if ( towns[i].name =="Fish Haven" ) {
+    let section = document.createElement('section');
+    let h4 = document.createElement('h4');
+    let x;
+    for (let x=0; x < towns[i].events.length; x++) {
+      h4.innerHTML += towns[i].events[x] + "<br>" ;
+      section.appendChild(h4);
+      
 
-    for (let i = 0; i < towns.length; i++ ) {
 
-        if (towns[i].name == "Fish Haven"){       
-			
-			const town = towns[i];
-
-			for (let x = 0; x < town.events.length; x++){
-
-				const images = ["event1.jpg", "event2.jpg", "event3.jpg", "event4.jpg"]
-
-				let div = document.createElement('div');
-				let event = document.createElement('p');
-				let image = document.createElement('img');
-
-				div.appendChild(event);
-				div.appendChild(image);
-
-				event.textContent = town.events[x];
-            	image.setAttribute('src', "assets/fishhaven-" + images[x]);
-				image.setAttribute('alt', town.events[x] + ", " + town.name + ", Idaho");
-				
-				document.querySelector('div.events').appendChild(div);
-			}
-
-        }
     }
-  });
+ 
+
+   
+
+document.querySelector('div.events').appendChild(section);
+}
+}
+});
