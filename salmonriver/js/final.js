54d3e402d0070dcd4831c15bfcc7d803a8a0f6c4
aliciaschreiner <aliciaschreiner@gmail.com>
2020-04-03 09:29:44 -0700

    a

--- salmonriver/js/final.js
+++ salmonriver/js/final.js
@@ -1,8 +1,43 @@
-/* Hamburger Menu*/
+/* Hamburger Menu */
 
 const hambutton = document.getElementsByClassName('hamburger')[0];
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+};
+
+/* Weather Summary */
+
+/* Weather Summary Riggins */
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4092267&units=imperial&APPID=e2c5cf2d76014478c0c017d1d040b23c";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    const weatherAPI = jsObject;
+ 
+    document.getElementById('current').textContent = weatherAPI.weather[0].main;
+    document.getElementById('temp').textContent = weatherAPI.main.temp_max.toFixed(1);
+    document.getElementById('humidity').textContent = weatherAPI.main.humidity;
+    document.getElementById('windSpeed').textContent = weatherAPI.wind.speed;   
+  });
+
+/* Wind Chill */
+
+let t = document.getElementById('temp').innerHTML;
+let s = document.getElementById('windSpeed').innerHTML;
+
+if ((t <= 50) && (s >= 3)) {
+
+let f = (35.74+0.6215*t) - (35.75*Math.pow(s,0.16)) +0.4275*t*(Math.pow(s,0.16));
+ 
+
+document.getElementById('windChill').innerHTML = Math.round(f);
+}
+
+else {
+document.getElementById('windChill').innerHTML = 'N/A';
 };
