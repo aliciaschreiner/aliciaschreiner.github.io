fed464288afda58340d79f711c8ca4cae2d11249
aliciaschreiner <aliciaschreiner@gmail.com>
2020-04-03 12:29:41 -0700

    a

--- salmonriver/js/final.js
+++ salmonriver/js/final.js
@@ -41,3 +41,46 @@ document.getElementById('windChill').innerHTML = Math.round(f);
 else {
 document.getElementById('windChill').innerHTML = 'N/A';
 };
+
+/* River Guide Json */
+
+const requestURL = 'https://aliciaschreiner.github.io/salmonriver/js/riverguides.json';
+ 
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  
+  .then(function (jsonObject) {
+    
+    const riverguides = jsonObject['riverguides'];
+   for (let i = 0; i < riverguides.length; i++ ) {
+    let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let certification = document.createElement('p');
+	  let experience = document.createElement('p');
+	  let email = document.createElement('p');
+	  let bio = document.createElement('p');
+      let image = document.createElement('img');
+
+
+
+h2.textContent = riverguides[i].name + ' ' + riverguides[i].lastname;
+certification.textContent = "Certification Level: " + riverguides[i].certification;
+experience.textContent = "Years of Experience: " + riverguides[i].experience;
+email.textContent = "Email: " + riverguides[i].email;
+bio.textContent = "Bio: " + riverguides[i].bio;
+image.setAttribute('src', riverguides[i].imageurl);
+image.setAttribute('alt', riverguides[i].name + ' ' + riverguides[i].lastname + '-' + [i]);
+
+
+card.appendChild(h2);
+card.appendChild(certification);
+card.appendChild(experience);
+card.appendChild(email);
+card.appendChild(bio);
+card.appendChild(image);
+
+document.querySelector('div.cards').appendChild(card);
+}
+});
