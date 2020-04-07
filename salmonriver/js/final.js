96bcf6098f2620487368644c5c3076298c912dc8
aliciaschreiner <aliciaschreiner@gmail.com>
2020-04-07 15:42:47 -0700

    a

--- salmonriver/js/final.js
+++ salmonriver/js/final.js
@@ -19,9 +19,8 @@ fetch(apiURL)
   
   //.then((response) => response.json())
   //.then((jsObject) => {
-	.then(function (jsonObject) {
-    console.log(jsObject);
-
+	.then(function (jsObject) {
+    
     const weatherAPI = jsObject;
  
     document.getElementById('current').textContent = weatherAPI.weather[0].main;
@@ -47,46 +46,3 @@ else {
 document.getElementById('windChill').innerHTML = 'N/A';
 };
 
-/* River Guide Json */
-
-const requestURL = 'https://aliciaschreiner.github.io/salmonriver/js/riverguides.json';
- 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  
-  .then(function (jsonObject) {
-    
-	const riverguides = jsonObject['riverguides']; 
-
-    for (let i = 0; i < riverguides.length; i++ ) {
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let certification = document.createElement('p');
-	let experience = document.createElement('p');
-	let email = document.createElement('p');
-	let bio = document.createElement('p');
-    let image = document.createElement('img');
-
-
-
-h2.textContent = riverguides[i].name + ' ' + riverguides[i].lastname;
-certification.textContent = "Certification Level: " + riverguides[i].certification;
-experience.textContent = "Years of Experience: " + riverguides[i].experience;
-email.textContent = "Email: " + riverguides[i].email;
-bio.textContent = "Bio: " + riverguides[i].bio;
-image.setAttribute('src', riverguides[i].imageurl);
-image.setAttribute('alt', riverguides[i].name + ' ' + riverguides[i].lastname + '-' + [i]);
-
-
-card.appendChild(h2);
-card.appendChild(certification);
-card.appendChild(experience);
-card.appendChild(email);
-card.appendChild(bio);
-card.appendChild(image);
-
-document.querySelector('div.cards').appendChild(card); 
-	}
-});
